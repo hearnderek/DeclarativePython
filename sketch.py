20aaa157144b2f65a3b845c42da775e1e10880fc
Derek Hearn <hearnderek@yahoo.com>
2021-04-30 19:04:13 +0900

    Update sketch.py
    Adding a lot of comments.
    I'm sure I'm missing some of the actual meat of the change, but the main thing was profiling and figuring out what is slow.
    It appears that pandas dataframe is a poor data structure for this workload. 
    A simple array with some helper indexer functions might be faster. 
    
    There's more orchestration functionality that I want to eventually bring in, but for now I am focusing on the slow calculation issue.

--- sketch.py
+++ sketch.py
@@ -80,6 +80,16 @@ using disk to lower memory usages with huge datasets
 
 
 class Run:
+    """
+    TODO:
+    1. reuse a run object with different inputs for faster runs through a big inforce.
+    2. bring in accumulation columns
+    3. multiprocess
+        currently inorder to run through a 112k inforce file for 35 year with monthly time steps it will take
+        2 days to fully process. 
+        To get a 30 minute runtime, we would need to split the process across 90 CPUs. (ignoring join time)
+    """
+
     def __init__(self, start_date, start_t=0, end_t=25, year_end_month=9):
         self.start_date = start_date
         self.start_t = start_t
@@ -91,6 +101,7 @@ class Run:
         self.last_update_time = None
         self.df = pd.DataFrame()
         self.__df_len__ = None
+        self.best_path = []
 
     def init_df(self, input: 'DataFrame') -> 'DataFrame': 
         df = input
@@ -122,156 +133,71 @@ class Run:
             self.df[col] = pd.NA
 
     
-    def calculate(self):
-        funcs = self.func_dict
-        g = Graph(funcs, self.get_df_len())
+    def calculate(self, best_path=None):
+        """
+        using the input columns, and all of the loaded functions calculate every single value.
 
-        sorted_cost_cols = list(g.fnodes.values())
-        sorted_cost_cols.sort(key=lambda x: x.cost)
+        Profile Results:
+            get_calc is our hot function.
+            The majority of our time is spent within pandas calls, unsurprisingly, getting and setting values.
+
+            It is quite possible that we would be better off with a different data-structure for this computation,
+            but then put our results back into pandas at the end. 
+
+            Another noticable bit is the pandas check isna. We might be better off using None instead for faster checks.
+
+        """
 
         self.start_time = time.time()
         self.last_update_time = time.time()
         print_progress_bar(0, 100, prefix = 'Progress:', suffix = 'Complete', length = 50)
-        for t in range(0, len(self.df)):
-            for col in [x.identifier for x in sorted_cost_cols]:
-                # visit all indicies and setting the values within underlying pandas dataframe
-                # All values are memozied, so only calculated once
-                # since it runs on recursive calls, dependency loops will fail (with poor error messages)
-                # since it runs on recurive calls, extremely deeply nested stacks might fail.
-                #   we could switch from a depth first to bredth first passes to defend against this.
-                #   - use generators instead of recursion? pushing all of the first needed params onto
-                #     a stack and working through that?
-                #     It would be ineffeciently passing through the graph, but it would give us more 
-                #     flexability in other areas of optimization. BUT, not worth it at thie point. KISS
-                #print(t, col)
+        
+        if best_path is not None:
+            # The best pass is something the user can pass into to speed up the calculation.
+            # The easiest way to do this is by letting one pass populate self.best_path,
+            # then pass that back in for repeated runs.
+            for (t, col) in best_path:
                 self.get_calc(t, col)
+        else:
+            # taking a calculated guess at a good path through our dependency graph
+            # Guess is based on a cost function, which gets bigger the further down the chain the calls are.
+            funcs = self.func_dict
+            g = Graph(funcs, self.get_df_len())
+
+            sorted_cost_cols = list(g.fnodes.values())
+            sorted_cost_cols.sort(key=lambda x: x.cost)
+
+            for col in [x.identifier for x in sorted_cost_cols]:
+                for t in range(0, self.get_df_len()):
+                    # visit all indicies and setting the values within underlying pandas dataframe
+                    # All values are memozied, so only calculated once
+                    # since it runs on recursive calls, dependency loops will fail (with poor error messages)
+                    # since it runs on recurive calls, extremely deeply nested stacks might fail.
+                    #   we could switch from a depth first to bredth first passes to defend against this.
+                    #   - use generators instead of recursion? pushing all of the first needed params onto
+                    #     a stack and working through that?
+                    #     It would be ineffeciently passing through the graph, but it would give us more 
+                    #     flexability in other areas of optimization. BUT, not worth it at thie point. KISS
+                    #
+                    # After an attempt to do bredth first pass, reverted back to this simple method.
+                    # since we're lazy in figuring out what index is needed  
+                    self.get_calc(t, col)
         
         seconds_elapsed = int((time.time() - self.start_time)*100)/100
         print_progress_bar(100, 100, prefix = 'Progress:', suffix = f'Complete　 {seconds_elapsed} sec', length = 50)
         return self.df
-
-
-    # def bredth_calculate(self):
-    #     funcs = self.func_dict
-    #     g = Graph(funcs, self.get_df_len())
-    #     g.print()
-    #     print()
-
-    #     sorted_cost_cols = list(g.fnodes.values())
-    #     sorted_cost_cols.sort(key=lambda x: x.cost)
-
-    #     self.start_time = time.time()
-    #     self.last_update_time = time.time()
-    #     print_progress_bar(0, 100, prefix = 'Progress:', suffix = 'Complete', length = 50)
-        
-    #     while self.df.isnull().values.any():
-    #         for t in range(0, self.get_df_len()):
-    #             for col in [x.identifier for x in sorted_cost_cols]:
-    #                 self.get_one_calc(t, col)
-        
-    #     seconds_elapsed = int((time.time() - self.start_time)*100)/100
-    #     print_progress_bar(100, 100, prefix = 'Progress:', suffix = f'Complete　 {seconds_elapsed} sec', length = 50)
-    #     return self.df
-
-
-    # def get_one_calc(self, t, col):
-    #     #print('get_calc', col, t)
-    #     if col == 't':
-    #         return t
-        
-    #     if t < 0 or t >= self.get_df_len():
-    #         # expected to be handled within user functions
-    #         return 'time out of range'
-
-        
-
-    #     val = self.df.at[t, col]
-    #     if pd.isna(val):
-    #         if col not in self.func_dict:
-    #             print(self.func_dict)
-    #             raise Exception('missing input or definition "' + col + '"')
-    #             print(f'missing input "{col}"')
-    #         else:
-    #             f = self.func_dict[col]
-                
-    #             values = []
-    #             needs = f.needs(t)
-    #             has_t = False
-    #             for (pcol, pt, ptype) in needs:
-                    
-
-    #                 if pcol == 't':
-    #                     values.append(t)
-    #                     has_t = True
-    #                 elif ptype == 'scaler':
-    #                     v = self.df.at[0, pcol]
-    #                     if pd.isna(v):
-    #                         #print('missing', 0, pcol)
-    #                         return pd.NA
-    #                     values.append(v)
-    #                 elif ptype == 'forward reference timeseries':
-    #                     at = max(0, min(pt+1, self.get_df_len()))
-    #                     v = self.df.at[at, pcol]
-    #                     if pd.isna(v):
-    #                         # print('--> missing', at, pcol)
-    #                         return pd.NA
-    #                     values.append(list(self.df[pcol].values))
-    #                 elif ptype == 'back reference timeseries':
-    #                     at = max(0, min(pt-1, self.get_df_len()))
-    #                     v = self.df.at[at, pcol]
-    #                     if pd.isna(v):
-    #                         # print('<- missing', at, pcol)
-    #                         return pd.NA
-    #                     values.append(list(self.df[pcol].values))
-    #                 elif ptype == 'timeseries':
-    #                     at = max(0, min(pt, self.get_df_len()))
-    #                     v = self.df.at[at, pcol]
-    #                     if pd.isna(v):
-    #                         # print('= missing', at, pcol)
-    #                         return pd.NA
-    #                     values.append(list(self.df[pcol].values))
-    #                 else:
-    #                     print('should not happen')
-
-                
-    #             if has_t:
-                    
-    #                 value = f.fn(*values)
-    #                 if col in watches:
-    #                     print('get_calc SET', col, values, '----', value)
-    #                 self.df.at[t, col] = value
-    #             else:
-    #                 value = f.fn(*values)
-    #                 if col in watches:
-    #                     print('get_calc SET', col, t, values, '----', value)
-    #                 self.df[col] = value
-                
-
-    #             self.calc_count += 1
-
-                
-                
-    #             if (time.time() - self.last_update_time) > 0.25: 
-    #                 # This is a pretty expensive operation
-    #                 # There is a noticable slowdown if checked at every 100th of a second
-
-
-    #                 completed = sum(list(self.df.count()))
-    #                 # print('completed', completed)
-    #                 total = self.df.shape[0] * self.df.shape[1]
-    #                 # print('total', total)
-
-    #                 seconds_elapsed = int((time.time() - self.start_time)*10)/10
-    #                 print_progress_bar(completed, total, prefix = 'Progress:', suffix = f'Complete　 {seconds_elapsed} sec', length = 50)
-    #                 self.last_update_time = time.time()
-
-    #             return value
-    #     else:
-    #         return val
-
             
 
     def get_calc(self, t, col):
+        """
+        Recursively get all the values needed to perform this calculation, save result (memoization), and return
+
+        For scaler values use t=0
+
+        TODO: optimize this, for it is the bottleneck.
+        - move away from string checks4
+        """
+
         #print('get_calc', col, t)
         if col == 't':
             return t
@@ -327,18 +253,20 @@ class Run:
                     if col in watches:
                         print('get_calc SET', col, values, '----', value)
                     self.df.at[t, col] = value
+                    self.best_path.append( (t, col) )
                 else:
                     value = f.fn(*values)
                     if col in watches:
                         print('get_calc SET', col, t, values, '----', value)
                     self.df[col] = value
+                    self.best_path.append( (-1, col) )
                 
 
                 self.calc_count += 1
 
                 
                 
-                if (time.time() - self.last_update_time) > 0.25: 
+                if (time.time() - self.last_update_time) > 1.0: 
                     # This is a pretty expensive operation
                     # There is a noticable slowdown if checked at every 100th of a second
 
@@ -357,6 +285,9 @@ class Run:
             return val
 
 class ImportedFunc:
+    """
+    Representation of a function loaded into our dependency calculation engine
+    """
 
     types = ['scaler', 'timeseries', 'back reference', 'forward reference', 'self reference']
 
@@ -369,16 +300,24 @@ class ImportedFunc:
         self.__is_cumulative__ = None
         self.steps = None
 
-    def f(self):
-        self.identifier
-
     def get_params(self):
+        # gets all variables then only takes the first ones because they are always arguments
         return self.fn.__code__.co_varnames[0:self.fn.__code__.co_argcount]
 
     def get_code(self):
         return inspect.getsource(self.fn)
 
     def get_type(self):
+        """
+        Parsing our function's code to determine how it fits within the dependency graph.
+        Currently only supports t+1, t-1
+        Constants will have undefined behavior, and [t] is an infiniate loop.
+
+        types:
+            scaler - same for all values of t
+            timeseries - has a value for every value of t
+            self reference - RECURSION but for different values of t
+        """
         if self.__type__:
             return self.__type__
 
@@ -411,6 +350,11 @@ class ImportedFunc:
 
 
     def get_param_types(self):
+        """
+        parsing the user defined code to better understand timeseries index usage.
+
+        Since code parsing of multiple functions is expensive, the result will be saved and returned on subsequent calls.
+        """
         if self.__param_types__:
             return self.__param_types__
         
@@ -422,6 +366,12 @@ class ImportedFunc:
 
     @staticmethod
     def __get_param_types__(param, code):
+        """
+        parsing the user defined code to better understand timeseries index usage.
+
+        TODO: better parsing to we can calculate for any value of t what index will be accessed
+            If we know this we can calculate a like optimal graph traversal.
+        """
         
         reg_identifer_timeseries_usages = param + '\[[^\]]*\]'
         timeseries_uses = re.findall(reg_identifer_timeseries_usages, code, re.MULTILINE)
@@ -472,6 +422,23 @@ class ImportedFunc:
 
     @staticmethod
     def get_functions(module: str):
+        """
+        Abusing the python interpreter to do our work.
+
+        Load in a specific module/file and return all local variables
+        Then keep only the functions.
+        ! HELPER FUNCTIONS:
+            should not be declared within the module.
+        
+            generally bad:
+                from mymodule import myfunction
+                myfunction(...)
+
+            use instead:
+                import mymodule
+                mymodule.myfunction(...)
+        """
+
         rets = {}
         exec("""
 import {module}
@@ -508,7 +475,23 @@ sets = [f + ' = {module}.' + f for f in fs]
 
 
 class Graph:
+    """
+    Using a dictionary list of ImportedFuncs, encapsulate with Fnodes, make it easier to work with our dependency graph.
+
+    If we want to add visualization logic, it will be added here.
+    """
+
     def __init__(self, function_dict, t=1):
+        """
+        function_dict:
+            dictionary using function identifiers as keys, and ImportedFuncs as values
+
+        t:
+            number of timeseries steps which will be used in our calculation. 
+            Used within cost calculation.
+            Ignore this unless you want to see how costs increase/decrease based on number of timesteps
+        """
+        
         self.funcs = list(function_dict.items())
         # self.funcs.sort(key=lambda x: len(x[1].get_params()))
         self.fnodes = dict([(identifier, Fnode(identifier, f)) for identifier, f in self.funcs])
@@ -519,8 +502,17 @@ class Graph:
 
         for identifier, f in self.fnodes.items():
             f.calculate_cost(t)
-            f.calculate_scost()
 
+
+    def get_heads(self):
+        """
+        a head is an fnode which is dependent on no other fnodes.
+
+        A node which is dependent on an input is still a head.
+        Inputs are not included within the dependency graph so they are not heads.
+        """
+        return [f for identifier, f in self.fnodes.items() if f.is_head()]
+        
     
     def print(self):
         values = list(self.fnodes.values())
@@ -529,7 +521,21 @@ class Graph:
             f.print()
             print()
 
+    def total_cost(self):
+        """
+        Give a cost for calculating every single value.
+
+        Can be done by making a node which takes every fnode as a parameter, then calculating the cost on that.
+        """
+        pass
+
 class Fnode:
+    """
+    Encapsulation of our ImportedFuncs, making them propper nodes of a unidirectional dependency graph.
+
+    We get cheeky with string manipulation since it was simple to implement, and it not being a detrement
+    to speed
+    """
     def __init__(self, identifier: str, f: ImportedFunc):
         self.identifier = identifier
         self.f = f
@@ -540,19 +546,35 @@ class Fnode:
         self.cost_no_param = ''
 
     def add_param(self, f:'Fnode'):
+        """
+        Any parameter not added in this fashion will be assumed to be input.
+        """
         self.params.append(f)
+
+    def param_names(self):
+        return [f.identifier for f in self.params]
 
     def calculate_cost(self, t=1):
         """
-        any params not added via add_param will be assumed to be an input
+        Calculate Cost of Function within Dependency Graph
+
+        This will walk through all nodes which this function depends on.
         """
+        if self.cost:
+            return self.cost
+        
         self.calculate_scost()
         self.cost = self.exec_simp_scost(t)
         return self.cost
 
     def calculate_scost(self):
         """
-        any params not added via add_param will be assumed to be an input
+        Calculate Cost of Function within Dependency Graph (with strings)
+
+        This will walk through all nodes which this function depends on.
+        Since our Run object ensures every value is only calculated once (memozation)
+        we want so ensure costs for multi-referenced functions are not double counted.
+        We do this by storing them in a unique set and summing those equations together.
         """
         if self.scost:
             return self.scost
@@ -575,14 +597,20 @@ class Fnode:
 
             self.scost += '+' + '+'.join(param_costs) 
 
-        return self.scost
-
-        return s    
+        return self.scost 
 
 
     def simplify_scost(self):
         """
         takes the long form equation generated by calculate_scost and simplifies it
+
+        returns: 
+            x*t + c
+
+            ex. 5*t + 2
+
+
+        Why: lots of ts and 1s added together isn't exactly readable...
         """
         t = 0
         one = 0
@@ -607,9 +635,30 @@ class Fnode:
         
 
     def exec_simp_scost(self, t):
+        """
+        Executing our simplified equation to get a cost
+
+        Yes, we could parse it easily, but so can python!
+        """
+
         rets = {}
         exec(f"t = {t}\nx = {self.simplify_scost()}", globals(), rets)
         return rets['x']
+
+    def is_head(self):
+        """
+        a head is a node is a function which is dependent on no other functions.
+
+        A node which is dependent on an input is still a head.
+        Inputs are not included within the dependency graph so they are not heads.
+        """
+        if self.exec_simp_scost(1) == 1:
+            return True
+        else:
+            return False
+
+    def num_inputs(self):
+        return len(self.f.get_params()) - len(self.params)
 
     def print(self):
         print(self.identifier + ":")
