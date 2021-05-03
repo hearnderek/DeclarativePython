c9ae8a5bebbf2fab434605160ba8a06c8f936d12
hearnderek <hearnderek@yahoo.com>
2021-05-03 11:02:57 +0900

    generated a highly nested test, to figure out optimizations

---
+++ test_gen/highlynested.py
@@ -0,0 +1,30 @@
+
+def f(cur, prev):
+    if prev:
+        d = f'def {cur}({prev}):\n' + \
+            f'    return {prev} + 1\n' + \
+            f'\n'
+    else:
+        d = f'def {cur}():\n' + \
+            f'    return 1\n' + \
+            f'\n'
+    return d
+
+def fnames(n):
+    if n > 0:
+        return (f'f{n}', f'f{n-1}')
+    else:
+    	return (f'f{n}', None)
+
+
+def gen(n):
+    funcs = [f(*fnames(i)) for i in range(n)]
+    for x in funcs:
+        print(x)
+        print()
+ 
+
+
+if __name__ == '__main__':
+     gen(5000)
+
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import context
 import declarative
+from time import time
 
 def test1():
     df = pd.read_csv('data/declarative_funcs.csv')
@@ -16,3 +17,22 @@ def test1():
     print(df)
 
     print(run.calculate())
+
+def test_highlynested():
+    func_dict = None
+    best_path = None
+    for n in range(100):
+        engine = declarative.Engine('', 350 * 12)
+        start = time()
+        if func_dict:
+            engine.func_dict = func_dict
+        else:
+            engine.process_module('highlynested')
+            func_dict = engine.func_dict
+        process_time = time() - start
+
+        start = time()
+        df = engine.calculate(best_path)
+        best_path = engine.best_path
+        calc_time = time() - start
+        print(process_time, calc_time)
